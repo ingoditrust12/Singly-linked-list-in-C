b30b066cd3bc614a080dd29868f8519c9ef5f358
root <franklinchilaka94@gmail.com>
2023-04-12 08:19:30 -0700

    singlylinked

---
+++ single.h
@@ -0,0 +1,23 @@
+#ifndef singly_linked
+#define singly_linked
+
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+
+
+   struct Node* head = NULL;
+   struct Node* second = NULL;
+   struct Node* third = NULL;
+   struct Node* fourth = NULL;
+   struct Node* fifth = NULL;
+   struct Node* sixth = NULL;
+   struct Node* seventh = NULL;
+   struct Node* eight = NULL;
+   struct Node* nine = NULL;
+   struct Node* ten = NULL;
+
+   #endif
+
